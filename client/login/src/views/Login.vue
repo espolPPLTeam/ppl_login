0272db83788f7173804fd2e78494661f9efb8443
edisonmora95 <edison_andre_9@hotmail.com>
2018-09-13 23:47:10 -0500

    Login

--- client/login/src/views/Login.vue
+++ client/login/src/views/Login.vue
@@ -1,46 +1,50 @@
 <template>
-  <v-card class="elevation-12">
-    <v-toolbar dark color="primary-espol" style="border-bottom: 2px solid #F5b400">
-      <v-toolbar-title class="mx-auto">Peer Project Learning</v-toolbar-title>
-    </v-toolbar>
-    <!--<v-responsive style="background-color: #eee">
-      <img src="@/assets/logo_espol.png" height="75px" class="mx-auto">
-    </v-responsive>-->
-    <v-card-text class="mt-3">
-      <v-form>
-        <v-text-field prepend-icon="person" name="login" label="Correo" type="text" v-model="email" :rules="[rules.required, rules.email]"></v-text-field>
-        <v-text-field prepend-icon="lock" name="password" label="Contraseña" id="password" type="password" v-model="password" :rules="[rules.required]"></v-text-field>
-      </v-form>
-    </v-card-text>
-    <v-card-actions class="mx-auto pb-3" style="justify-content: center;">
-      <v-container grid-list-xl fluid>
-        <v-layout wrap class="text-xs-center">
-          <v-flex xs12 sm6>
-            <v-btn class="primary-espol" :loading="loading" :disabled="disabled" @click.native="login">Iniciar sesión</v-btn>
-          </v-flex>
-          <v-flex xs12 sm6>
-            <v-menu offset-y>
-              <v-btn class="btn-registrar" slot="activator">Registrar</v-btn>
-              <v-list>
-                <v-list-tile @click="registroProfesor">
-                  <v-list-tile-title>Profesor</v-list-tile-title>
-                </v-list-tile>
-                <v-list-tile @click="registroEstudiante">
-                  <v-list-tile-title>Estudiante</v-list-tile-title>
-                </v-list-tile>
-              </v-list>
-            </v-menu>
-          </v-flex>
-        </v-layout>
-      </v-container>
-      
-      <!--<v-btn class="btn-registrar">Registrar</v-btn>-->
-      
-    </v-card-actions>
-    <!--<v-card-actions class="mt-3">
-      <p class="mx-auto">¿No tienes usuario? <a href="/registro"> Regístrate aquí!</a></p>      
-    </v-card-actions>-->
-  </v-card>
+  <main>
+    <v-card class="elevation-12">
+      <v-toolbar dark color="primary-espol" style="border-bottom: 2px solid #F5b400">
+        <v-toolbar-title class="mx-auto">Peer Project Learning</v-toolbar-title>
+      </v-toolbar>
+      <v-card-text class="mt-3">
+        <v-form>
+          <v-text-field prepend-icon="person" name="login" label="Correo" type="text" v-model="email" :rules="[rules.required, rules.email]"></v-text-field>
+          <v-text-field prepend-icon="lock" name="password" label="Contraseña" id="password" type="password" v-model="password" :rules="[rules.required]"></v-text-field>
+        </v-form>
+      </v-card-text>
+      <v-card-actions class="mx-auto pb-3" style="justify-content: center;">
+        <v-container grid-list-xl fluid>
+          <v-layout wrap class="text-xs-center">
+            <v-flex xs12 sm6>
+              <v-btn class="primary-espol" :loading="loading" :disabled="disabled" @click.native="login">Iniciar sesión</v-btn>
+            </v-flex>
+            <v-flex xs12 sm6>
+              <v-menu offset-y>
+                <v-btn class="btn-registrar" slot="activator">Registrar</v-btn>
+                <v-list>
+                  <v-list-tile @click="registroProfesor">
+                    <v-list-tile-title>Profesor</v-list-tile-title>
+                  </v-list-tile>
+                  <v-list-tile @click="registroEstudiante">
+                    <v-list-tile-title>Estudiante</v-list-tile-title>
+                  </v-list-tile>
+                </v-list>
+              </v-menu>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card-actions>
+    </v-card>
+    <v-dialog v-model="dialog.active" width="500">
+      <v-card class="text-xs-center">
+        <v-card-text class="headline">{{ dialog.text }}</v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn flat @click="dialog.active = false">
+            Aceptar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </main>
 </template>
 <script>
   export default {
@@ -55,6 +59,10 @@
             const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
             return pattern.test(value) || 'Email inválido'
           }
+        },
+        dialog: {
+          active: false,
+          text: ''
         }
       }
     },
@@ -66,15 +74,31 @@
     methods: {
       login () {
         this.loading = true
-        setTimeout(() => {
-          this.loading = false
-        }, 3000)
+        const url = '/api/login/session'
+        this.$http.post(url, { email: this.email, password: this.password })
+          .then((resp) => {
+            this.loading = false
+            this.dialogSuccess()
+            localStorage.setItem('x-access-token', resp.body.datos)
+          })
+          .catch((err) => {
+            this.loading = false
+            this.dialogError(err.body.message)
+          })
       },
       registroProfesor () {
         this.$router.push('/registro/profesor')
       },
       registroEstudiante () {
         this.$router.push('/registro/estudiante')
+      },
+      dialogSuccess () {
+        this.dialog.active = true
+        this.dialog.text = 'Exito'
+      },
+      dialogError (text) {
+        this.dialog.active = true
+        this.dialog.text = text
       }
     }
   }
