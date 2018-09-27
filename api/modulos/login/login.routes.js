1df99982bb6ca99483ee7be589c15c65e8dda10b
edisonmora95 <edison_andre_9@hotmail.com>
2018-09-27 08:17:57 -0500

    Get session jwt

--- api/modulos/login/login.routes.js
+++ api/modulos/login/login.routes.js
@@ -1,7 +1,8 @@
 const LoginController = require('./login.controller')
+const Auth = require('../authentication/authentication')
 
 module.exports = function (app, opts, next) {
-  
+
   app.post('/session', async (req, res) => {
     let resp = await LoginController.login(req.body.email, req.body.password)
     return { datos: resp }
@@ -10,6 +11,16 @@ module.exports = function (app, opts, next) {
   app.get('/session/:email', async (req, res) => {
   	let resp = await LoginController.obtenerUsuarioPorEmail(req.params.email)
   	return { datos: resp }
+  })
+
+  app.get('/session', async (req, res) => {
+    try{
+      const decoded = await Auth.verifyToken(req)
+      let resp = await LoginController.obtenerUsuarioPorEmail(decoded.email)
+      return { datos: resp }
+    } catch (err) {
+      return { err: err }
+    }
   })
 
   next()
